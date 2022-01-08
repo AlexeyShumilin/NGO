4db17e7995274e38e0af924b9031fe318782e296
Alexey Shumilin <shumil.alexey@gmail.com>
2022-01-08 12:11:27 +0200

    initial

---
+++ components/navigation/Logo.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>
+    {{ title }}
+  </h1>
+</template>
+
+<script>
+  export default {
+    props:{
+       title: {
+         type: String,
+         default: '',
+
+       }
+    }
+
+  }
+</script>
+
+<style scoped>
+
+</style>
